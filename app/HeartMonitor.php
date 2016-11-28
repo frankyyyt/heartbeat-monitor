0bfae3407feb63a3284b10001d00da3854693fcb
Gordon Anderson <gordon@ginja.co.th>
2016-11-28 06:09:27 +0000

    MINOR: Core reformat, default values, @channel slack

--- app/HeartMonitor.php
+++ app/HeartMonitor.php
@@ -8,30 +8,31 @@ use Dotenv\Dotenv;
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
 
-class HeartMonitor {
-	public function __construct() {
-		$this->env = new Dotenv(__DIR__);
-		$this->env->load();
+class HeartMonitor
+{
+    public function __construct()
+    {
+        $this->env = new Dotenv(__DIR__);
+        $this->env->load();
 
         $this->apiUrl = getenv('APP_URL');
 
         $this->log = new Logger('heartbeat');
         $this->log->pushHandler(new StreamHandler('logs/heartbeat.log', Logger::INFO));
-
     }
 
-	public function check() {
-		$this->guestToken = $this->getGuestToken();
-		$heartbeatDate = $this->getNewestHeartBeatDate();
+    public function check()
+    {
+        $this->guestToken = $this->getGuestToken();
+        $heartbeatDate = $this->getNewestHeartBeatDate();
         $newestHeartBeatString = $this->getNewestHeartBeatDate();
-        $timestampFile = getenv('TIMESTAMP_FILE');
+        $timestampFile = $this->getEnvWithDefault('TIMESTAMP_FILE', '.heartbeat');
 
         // no timestamp file means first time this has run
         if (!file_exists($timestampFile)) {
             // Write the contents back to the file
             file_put_contents($timestampFile, $newestHeartBeatString);
             $this->log->info('First hearbeat for ' . $this->apiUrl);
-
         } else {
             $previousHeartBeatString = file_get_contents($timestampFile);
 
@@ -52,10 +53,23 @@ class HeartMonitor {
     }
 
     /*
+     * Get a property from .env, but default if not specified
+     */
+    private function getEnvWithDefault($key, $default)
+    {
+        $result = getenv($key);
+        if (empty($result)) {
+            $result = $default;
+        }
+        return $result;
+    }
+
+    /*
      * Send a slack notification, but avoid notification spam by only sending every 60 mins
      */
-    private function notifyQueueBroken() {
-        $propertiesFile = getenv('PROPERTIES_FILE');
+    private function notifyQueueBroken()
+    {
+        $propertiesFile = $this->getEnvWithDefault('PROPERTIES_FILE', '.properties');
         $properties = file_exists($propertiesFile) ? json_decode(file_get_contents($propertiesFile)) : [];
         $sendMessage = true;
 
@@ -77,7 +91,7 @@ class HeartMonitor {
         }
 
         if ($sendMessage) {
-            $message = 'The queue for API server ' . $this->apiUrl . 'appears to have stopped working';
+            $message = '@channel The queue for API server ' . $this->apiUrl . 'appears to have stopped working';
             $this->sendSlackNotifiations($message);
         }
     }
@@ -85,8 +99,9 @@ class HeartMonitor {
     /*
      * Delete the properties file if it exists
      */
-    private function deletePropertiesFile() {
-        $propertiesFile = getenv('PROPERTIES_FILE');
+    private function deletePropertiesFile()
+    {
+        $propertiesFile = $this->getEnvWithDefault('PROPERTIES_FILE', '.properties');
         if (file_exists($propertiesFile)) {
             unlink($propertiesFile);
         }
@@ -111,12 +126,12 @@ class HeartMonitor {
         }
     }
 
-	/*
+    /*
 	 * Get the oauth guest token
 	 */
-	private function getGuestToken()
+    private function getGuestToken()
     {
-    	$clientSecret = getenv('STORE_FRONT_CLIENT_SECRET');
+        $clientSecret = getenv('STORE_FRONT_CLIENT_SECRET');
         $params = [
             'grant_type'    => 'guest',
             'client_id'     => 'store-front',
@@ -138,8 +153,8 @@ class HeartMonitor {
      */
     private function getNewestHeartBeatDate()
     {
-    	$endpoint = $this->apiUrl . getenv('SETTINGS_ENDPOINT') . '?access_token=' . $this->guestToken;
-    	$guzzle = new \GuzzleHttp\Client();
+        $endpoint = $this->apiUrl . getenv('SETTINGS_ENDPOINT') . '?access_token=' . $this->guestToken;
+        $guzzle = new \GuzzleHttp\Client();
         $response = $guzzle->request('GET', $endpoint);
         $settings = json_decode($response->getBody(), true);
         return $settings['data']['heartbeat_date'];
